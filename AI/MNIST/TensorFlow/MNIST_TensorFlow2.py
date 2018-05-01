6f326ed8cc1990d40cdbb4e463777535db316f53
IL-RL <IL.ReXe.RL@gmail.com>
2018-05-02 06:17:57 +0900

    Leaky_ReLU

--- AI/MNIST/TensorFlow/MNIST_TensorFlow2.py
+++ AI/MNIST/TensorFlow/MNIST_TensorFlow2.py
@@ -66,14 +66,14 @@ b_conv1 = bias_variable([32])
 
 x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_conv1 = tf.nn.leaky_relu(conv2d(x_image, W_conv1) + b_conv1, alpha=-0.1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 # 두 번째 합성곱
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_conv2 = tf.nn.leaky_relu(conv2d(h_pool1, W_conv2) + b_conv2, alpha=-0.1)
 h_pool2 = max_pool_2x2(h_conv2)
 
 # Fully-Connected Layer
@@ -81,7 +81,7 @@ W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_fc1 = tf.nn.leaky_relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1, alpha=-0.1)
 
 # 과적합 방지, Dropout
 keep_prob = tf.placeholder(tf.float32)
